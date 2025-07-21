b37e3e43f70cc49aa46ebf074509edddd27239c0
wayueee <farhanhokila30@gmail.com>
2025-07-21 12:22:29 +0700

    fix responsive

--- pages/index.vue
+++ pages/index.vue
@@ -10,8 +10,26 @@
   </div>
 </template>
 
-<script lang="ts" setup>
+<script>
+import Navbar from "~/components/Navbar.vue";
+import IntroSection from "~/components/IntroSection.vue";
+import Portofolio from "~/components/Portofolio.vue";
+import HowToUse from "~/components/HowToUse.vue";
+import Pricing from "~/components/Pricing.vue";
+import FaqSection from "~/components/FaqSection.vue";
+import Footer from "~/components/Footer.vue";
 
+export default {
+  components:{
+    Navbar,
+    IntroSection,
+    Portofolio,
+    HowToUse,
+    Pricing,
+    FaqSection,
+    Footer,
+  }
+};
 </script>
 
 <style></style>
